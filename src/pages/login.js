cf9fe592ad398984a7a47e4ac2616f160b000488
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-03-17 12:27:30 +0530

    added bare bone hooks to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,23 @@
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+import { useState, useEffect, useContext } from "react";
+
 export default function Login() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+
+    const handleLogin = () => { };
+
+    useEffect(() => {
+        document.title = 'Login - Instagram';
+    }, []);
+
     return (
         <p>I am the login page</p>
     );
