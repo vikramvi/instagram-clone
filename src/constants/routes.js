f35649000ad39765ac9e87ebf2bf127b47e0e1ef
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-03-17 11:47:19 +0530

    dummy login page with react routers, lazy loading

---
+++ src/constants/routes.js
@@ -0,0 +1,5 @@
+export const DASHBOARD = "/";
+export const SIGNUP = "/signup";
+export const LOGIN = "/login";
+export const PROFILE = "/p/:username";
+export const NOT_FOUND = "/not-found";
---
+++ src/pages/login.js
@@ -0,0 +1,5 @@
+export default function Login() {
+    return (
+        <p>I am the login page</p>
+    );
+};
