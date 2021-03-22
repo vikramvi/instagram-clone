5e818507a44d6979bb7bb98115c3c0953041f093
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-03-22 15:44:23 +0530

    dashboard's sidebar   implementation completed along with firebase integration

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,14 @@
+import useUser from "../../hooks/use-user";
+import Suggestions from "./suggestions";
+import User from "./user";
+
+export default function Sidebar() {
+    const { user: { fullName, username, userId } } = useUser();
+
+    return (
+        <div className="p-4">
+            <User username={username} fullName={fullName} />
+            <Suggestions />
+        </div>
+    )
+}
