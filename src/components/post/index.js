6a469bb086a27f458373e3f457737c19187a5b64
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-03-27 17:53:03 +0530

    post > footer section added

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Header from "./header";
 import Image from "./image";
 import Actions from "./actions";
+import Footer from "./footer";
 
 
 export default function Post({ content }) {
@@ -22,6 +23,7 @@ export default function Post({ content }) {
                 likedPhoto={content.userLikedPhoto}
                 handleFocus={handleFocus}
             />
+            <Footer caption={content.caption} username={content.username} />
         </div>
     );
 }
