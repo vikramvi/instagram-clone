5a93f009b2ec5355bbdee9d97a84a5929053a889
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2021-03-27 17:23:30 +0530

    picture like / unlike logic implemented

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,15 +2,26 @@ import { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
 import Image from "./image";
+import Actions from "./actions";
 
 
 export default function Post({ content }) {
     //console.log("content", content);
+
+    const commentInput = useRef(null);
+    const handleFocus = () => commentInput.current.focus();
+
     //Components - header, image, actions ( like & comment icons), footer, comments
     return (
         <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
             <Header username={content.username} />
             <Image src={content.imageSrc} caption={content.caption} />
+            <Actions
+                docId={content.docId}
+                totalLikes={content.likes.length}
+                likedPhoto={content.userLikedPhoto}
+                handleFocus={handleFocus}
+            />
         </div>
     );
 }
